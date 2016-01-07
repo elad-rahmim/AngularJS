62814aa8a8f4135b5f16121b7c00ca1e2c1c166d
elad.rahamim <elad.rahamim@winner.com>
2016-01-07 15:51:00 +0200

    start load data
    start load data

--- Dynamic_Pages_1/app/scripts/controllers/main.js
+++ Dynamic_Pages_1/app/scripts/controllers/main.js
@@ -8,10 +8,24 @@
  * Controller of the dynamicPages1App
  */
 angular.module('dynamicPages1App')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($http) {
+    var main = this;
+      $http.get('http://loc.canwin.co.il/wp-json/wp/v2/pages?filter[name]=sample-page')
+          .then(getPageSuccess);
+
+      function getPageSuccess(response) {
+        console.log('success: ', response);
+        // NEW CODE HERE
+        if (response.data && response.data[0]) {
+          if (response.data[0].title) {
+            main.title = response.data[0].title.rendered;
+          }
+          if (response.data[0].content) {
+            main.content = response.data[0].content.rendered;
+          }
+        } else {
+          throw response;
+        }
+        console.log('main: ', main);
+      }
   });
