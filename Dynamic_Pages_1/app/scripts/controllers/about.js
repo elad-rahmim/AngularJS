2b567a8126b3a1f19b71f2cda5907bcd84dcbc04
elad.rahamim <elad.rahamim@winner.com>
2016-01-07 14:50:56 +0200

    create project for  Dynamic_Pages_1
    create project for  Dynamic_Pages_1

---
+++ Dynamic_Pages_1/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dynamicPages1App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dynamicPages1App
+ */
+angular.module('dynamicPages1App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ Dynamic_Pages_1/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dynamicPages1App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dynamicPages1App
+ */
+angular.module('dynamicPages1App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
